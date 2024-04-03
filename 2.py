0cf1419f06b5e88e5e8a684f1c5fe45f35e3e654
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-04-03 10:57:52 +0530

    first commit

---
+++ 2.py
@@ -0,0 +1,8 @@
+x=list('Python')
+for i in enumerate(x):
+    print(i,end=" ")
+
+#(0, 'P') (1, 'y') (2, 't') (3, 'h') (4, 'o') (5, 'n')
+#initially x is holding a list as Python
+#enumerate will convert it into tuple of indexes
+#and its getting printed.
